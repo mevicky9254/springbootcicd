06750daeed388c03884133f6e4144bb5c1601198
vikki <vikkichoudhary6528@gmail.com>
2026-04-13 17:43:31 +0530

    first commit

---
+++ springbootcicd/src/main/java/com/example/springbootcicd/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.springbootcicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Spring Boot is alive 🚀";
+    }
+
+    @GetMapping("/greet")
+    public String greetUser(@RequestParam(defaultValue = "Guest") String name) {
+        return "Hello, " + name + "!";
+    }
+}
